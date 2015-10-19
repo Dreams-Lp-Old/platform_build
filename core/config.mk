0f745db0e3bef151cb020fff794e1a4d9453528c
Carlos Manuel Leemet <carlosmanuel_leemet@msn.com>
2015-10-19 14:39:02 +0300

    Update config.mk

--- core/config.mk
+++ core/config.mk
@@ -618,6 +618,6 @@ RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcomp
 
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-include $(TOPDIR)vendor/pa/sepolicy/sepolicy.mk
+include $(TOPDIR)vendor/ds/sepolicy/sepolicy.mk
 
 include $(BUILD_SYSTEM)/dumpvar.mk
